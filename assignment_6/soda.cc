31c9e91ac7468ee5a0a787514f7f1d3d869a68bd
leigh <leigh@leigh-VirtualBox.(none)>
2013-11-30 19:46:13 -0500

    A6 main and makefile

---
+++ assignment_6/soda.cc
@@ -0,0 +1,98 @@
+#include "BottlingPlant.h"
+#include "VendingMachine.h"
+#include "NameServer.h"
+#include "Student.h"
+#include "Parent.h"
+#include "Bank.h"
+#include "WATCardOffice.h"
+#include "ConfigParams.h"
+#include "MPRNG.h"
+
+#include <iostream>
+
+using namespace std;
+
+void usageQuit(const char* cmd) {
+  cout<<"Usage: "<<cmd
+      <<" [ config-file [ random-seed (> 0) ] ]"<<endl;
+  exit(0);
+}
+
+int doConvert(const char* str, int minValue, const char* cmd) {
+  char* end;
+  int res = strtol(str, &end, 10);
+  if ((const char*)end == str || res < minValue) {
+    usageQuit(cmd);
+  }
+  return res;
+}
+
+void uMain::main() {
+  unsigned int seed = getpid();
+  const char* configFile = "soda.config";
+  switch (argc) {
+    case 3:
+      seed = doConvert(argv[2], 0, argv[0]);
+    case 2:
+      configFile = argv[1];
+    case 1:
+      break;
+    default:
+      usageQuit(argv[0]);
+  }
+  ConfigParams conf;
+  processConfigFile(configFile, params);
+
+  mprng.seed(seed);
+
+  {
+    Printer printer(
+        conf.numStudents,
+        conf.numVendingMachines,
+        conf.numCouriers);
+    Bank bank(conf.numStudents);
+    Parent parent(printer, bank, conf.numStudents, conf.parentalDelay);
+    NameServer nameServer(printer, conf.numVendingMachines, conf.numStudents);
+    WATCardOffice cardOffice(printer, bank, conf.numCouriers);
+    vector<VendingMachine*> machines;
+    {
+      BottlingPlant bottlingPlant(
+          printer,
+          conf.numVendingMachines,
+          conf.maxShippedPerFlavour,
+          conf.maxStockPerFlavour,
+          conf.timeBetweenShipments);
+
+      vector<Student*> students;
+      for (int i = 0; i < conf.numStudents; ++i) {
+        students.push_back(new Student(
+            printer,
+            nameServer,
+            cardOffice,
+            i,
+            conf.maxPurchases));
+      }
+
+      for (int i = 0; i < conf.numVendingMachines; ++i) {
+        machines.push_back(new VendingMachine(
+            printer,
+            nameserver,
+            i,
+            conf.sodaCost,
+            conf.maxStockPerFlavour));
+      }
+
+      // wait for the students to terminate first
+      for (int i = 0; i < students.size(); ++i) {
+        delete students[i];
+      }
+      // then wait for the plant+truck
+    }
+    // now it's safe for the machines to terminate
+    for (int i = 0; i < machines.size(); ++i) {
+      delete machines[i];
+    }
+    // wait for everything else to terminate
+  }
+  cout<<"***********************"<<endl;
+}
